7cbca72dcddf0cc9aea790d6e43a2e682d4e1592
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-02-10 21:14:38 +0100

    :sparkles: Little squares moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
-#include <stdlib.h>
+#include <vcruntime.h>
+#include <cstdlib>
+#include <random>
+#include <vector>
+#include "glm/fwd.hpp"
 #include "p6/p6.h"
+
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
 
@@ -14,17 +19,35 @@ int main(int argc, char* argv[])
             return EXIT_SUCCESS;
     }
 
+    // Squares infos
+    size_t nbSquare = 100;
+
+    std::vector<glm::vec2>                coordSquare(nbSquare);
+    std::vector<glm::vec2>                directionSquare(nbSquare);
+    std::default_random_engine            gen;
+    std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+    for (size_t i = 0; i < nbSquare; ++i)
+    {
+        coordSquare[i]     = glm::vec2(distribution(gen) * 2.0f, distribution(gen));
+        directionSquare[i] = glm::vec2(distribution(gen) / 10, distribution(gen) / 10);
+    }
+
     // Actual app
-    auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
+    auto ctx = p6::Context{{.title = "Prog-S4"}};
     ctx.maximize_window();
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+
+        for (size_t i = 0; i < nbSquare; ++i)
+        {
+            coordSquare[i] += directionSquare[i] * 0.1f;
+            ctx.square(
+                p6::Center{coordSquare[i]},
+                p6::Radius{0.1f}
+            );
+        }
     };
 
     // Should be done last. It starts the infinite loop.
