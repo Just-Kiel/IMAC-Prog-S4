2d97853dc3518b3f360faed4a45ea0860382f574
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-03-01 15:52:52 +0100

    :recycle: Clean with Jules

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <vcruntime.h>
 #include <cstdlib>
 #include <random>
 #include <vector>
@@ -10,6 +11,28 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
 
+std::vector<Boid> createBoids(p6::Context& ctx)
+{
+    size_t nbBoids = 2;
+
+    std::default_random_engine            gen;
+    std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+
+    // Boids init
+    std::vector<Boid> allBoids;
+
+    for (size_t i = 0; i < nbBoids; ++i)
+    {
+        allBoids.emplace_back(
+            glm::vec3(distribution(gen) * ctx.aspect_ratio(), distribution(gen), 0),
+            p6::Radius(0.1f),
+            static_cast<p6::Rotation>(2.0_radians * p6::PI * distribution(gen))
+        );
+    }
+
+    return allBoids;
+}
+
 int main(int argc, char* argv[])
 {
     { // Run the tests
@@ -21,43 +44,38 @@ int main(int argc, char* argv[])
             return EXIT_SUCCESS;
     }
 
-    // Squares infos
-    int   nbSquare = 100;
-    float speed    = 0.001f;
-
-    std::vector<glm::vec2>                coordSquare(nbSquare);
-    std::vector<glm::vec2>                directionSquare(nbSquare);
+    // Speed info
+    float                                 speed = 0.001f;
     std::default_random_engine            gen;
     std::uniform_real_distribution<float> distribution(-1.0, 1.0);
-    for (size_t i = 0; i < nbSquare; ++i)
-    {
-        coordSquare[i]     = glm::vec2(distribution(gen) * 2.0f, distribution(gen));
-        directionSquare[i] = glm::vec2((distribution(gen) > 0) ? 1 : -1, (distribution(gen) > 0) ? 1 : -1);
-    }
-
-    // Boids test
-    // TODO(Aurore): maybe not init there
-    std::vector<Boid> allBoids;
-
-    for (size_t i = 0; i < nbSquare; ++i)
-    {
-        allBoids.emplace_back(
-            glm::vec3(coordSquare[i], 0),
-            p6::Radius(0.1f),
-            static_cast<p6::Rotation>(2.0_radians * p6::PI * distribution(gen))
-        );
-    }
 
     // Actual app
     auto ctx = p6::Context{{.title = "Projecto con Olivia"}};
     ctx.maximize_window();
+
+    // Boids instance
+    std::vector<Boid> allBoids = createBoids(ctx);
 
     // ImGui informations
     ctx.imgui = [&]() {
         // Show a simple window
         ImGui::Begin("Boids parameters");
         ImGui::SliderFloat("Boids speed", &speed, 0.f, 2.f);
-        ImGui::SliderInt("Boids number", &nbSquare, 0, 500);
+
+        {
+            int nbBoids = allBoids.size();
+            if (ImGui::SliderInt("Boids number", &nbBoids, 0, 500))
+            {
+                if (nbBoids > allBoids.size())
+                {
+                    allBoids.emplace_back(glm::vec3(distribution(gen) * ctx.aspect_ratio(), distribution(gen), 0), p6::Radius(0.1f), static_cast<p6::Rotation>(2.0_radians * p6::PI * distribution(gen)));
+                }
+                else
+                {
+                    allBoids.pop_back();
+                }
+            }
+        }
         ImGui::End();
     };
 
@@ -65,21 +83,10 @@ int main(int argc, char* argv[])
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
 
-        if (nbSquare != allBoids.size())
-        {
-            if (nbSquare > allBoids.size())
-            {
-                allBoids.emplace_back(glm::vec3(distribution(gen) * 2.0f, distribution(gen), 0), p6::Radius(0.1f), static_cast<p6::Rotation>(2.0_radians * p6::PI * distribution(gen)));
-            }
-            else
-            {
-                allBoids.pop_back();
-            }
-        }
-
         for (auto& boid : allBoids)
         {
             boid.updateCenter(speed, allBoids);
+            boid.avoidWalls(ctx);
             // boid.updateDir(speed, static_cast<p6::Angle>(0.05_radians * p6::PI * distribution(gen)));
         }
 
